987c228988ce35aa834b4a2f2e0f3a947bc10a9c
Roman Pavlov <rspaulau@gmail.com>
2018-05-26 15:14:09 +0300

    add .sln and Domain project

---
+++ server/BorrowBuddy.Domain/Flow.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace BorrowBuddy.Domain
+{
+  public class Flow
+  {
+    public long Id { get; set; }
+    public DateTimeOffset Timestamp { get; set; }
+    public Participant Lender { get; set; }
+    public Participant Lendee { get; set; }
+    public decimal Amount { get; set; }
+    public string Comment { get; set; }
+  }
+}
---
+++ server/BorrowBuddy.Domain/Participant.cs
@@ -0,0 +1,10 @@
+namespace BorrowBuddy.Domain
+{
+  public class Participant
+  {
+    public long Id { get; set; }
+    public string FirstName { get; set; }
+    public string MiddleName { get; set; }
+    public string LastName { get; set; }
+  }
+}
