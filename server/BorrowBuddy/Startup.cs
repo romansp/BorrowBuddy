e88ad740ba0ef41fdba959605728ec2af152558a
Roman Pavlov <rspaulau@gmail.com>
2018-06-03 17:23:32 +0300

    add swagger via swashbuckle

--- server/BorrowBuddy/Startup.cs
+++ server/BorrowBuddy/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace BorrowBuddy {
   public class Startup {
@@ -21,10 +22,14 @@ namespace BorrowBuddy {
       services.AddDbContextPool<BorrowBuddyContext>(options => options
               .UseLazyLoadingProxies()
               .UseSqlServer(Configuration.GetConnectionString("BorrowBuddyContext")));
+
+      services.AddSwaggerGen(c => {
+        c.SwaggerDoc("api", new Info { Title = "BorrowBuddy" });
+      });
     }
 
     public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
-      if (env.IsDevelopment()) {
+      if(env.IsDevelopment()) {
         app.UseDeveloperExceptionPage();
         app.UseCors(builder =>
         builder.AllowAnyOrigin()
@@ -37,6 +42,11 @@ namespace BorrowBuddy {
       }
       app.UseHttpsRedirection();
       app.UseMvc();
+      app.UseSwagger();
+      app.UseSwaggerUI(c => {
+        c.SwaggerEndpoint("/swagger/api/swagger.json", "BorrowBuddy");
+      });
+
     }
   }
 }
