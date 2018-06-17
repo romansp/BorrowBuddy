4f757a65b8fb0457e11fe190d30e8fb8bab6a0d1
Roman Pavlov <rspaulau@gmail.com>
2018-06-18 02:43:31 +0300

    enable static file serving

--- server/BorrowBuddy/Startup.cs
+++ server/BorrowBuddy/Startup.cs
@@ -8,55 +8,47 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.AspNetCore.Swagger;
 
-namespace BorrowBuddy
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddTransient<CurrencyService>();
-            services.AddTransient<ParticipantService>();
-            services.AddTransient<FlowService>();
-            services.AddTransient<BalanceService>();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddCors(options =>
-            {
-                options.AddPolicy("CorsPolicy",
-              builder => builder.AllowAnyOrigin()
-              .AllowAnyMethod()
-              .AllowAnyHeader()
-              .AllowCredentials());
-            });
-
-            services.AddDbContextPool<BorrowBuddyContext>(options => options
-                    .UseLazyLoadingProxies()
-                    .UseSqlServer(Configuration.GetConnectionString("BorrowBuddyContext")));
-
-            services.AddSwaggerGen(c => c.SwaggerDoc("api", new Info { Title = "BorrowBuddy" }));
-        }
-
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseCors("CorsPolicy");
-            app.UseMvc();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/api/swagger.json", "BorrowBuddy"));
-        }
+namespace BorrowBuddy {
+  public class Startup {
+    public Startup(IConfiguration configuration) {
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services) {
+      services.AddTransient<CurrencyService>();
+      services.AddTransient<ParticipantService>();
+      services.AddTransient<FlowService>();
+      services.AddTransient<BalanceService>();
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+      services.AddCors(options => {
+        options.AddPolicy("CorsPolicy",
+      builder => builder.AllowAnyOrigin()
+      .AllowAnyMethod()
+      .AllowAnyHeader()
+      .AllowCredentials());
+      });
+
+      services.AddDbContextPool<BorrowBuddyContext>(options => options
+              .UseLazyLoadingProxies()
+              .UseSqlServer(Configuration.GetConnectionString("BorrowBuddyContext")));
+
+      services.AddSwaggerGen(c => c.SwaggerDoc("api", new Info { Title = "BorrowBuddy" }));
+    }
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+      app.UseStaticFiles();
+      if(env.IsDevelopment()) {
+        app.UseDeveloperExceptionPage();
+      } else {
+        app.UseHsts();
+      }
+      app.UseHttpsRedirection();
+      app.UseCors("CorsPolicy");
+      app.UseMvc();
+      app.UseSwagger();
+      app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/api/swagger.json", "BorrowBuddy"));
+    }
+  }
 }
