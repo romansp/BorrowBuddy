69aca06192f122d9df7f201df4fa37bbc48b6507
Roman Pavlov <rspaulau@gmail.com>
2018-06-23 20:02:17 +0300

    participants list/admin participants view

---
+++ client/src/components/ParticipantsList.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-list subheader>
+    <v-subheader>Users</v-subheader>
+    <v-list-tile 
+      v-for="item in items" 
+      :key="item.firstName" 
+      avatar 
+      @click="edit">
+      <v-list-tile-content>
+        <v-list-tile-title v-text="item.firstName"/>
+      </v-list-tile-content>
+      <v-list-tile-action>
+        <v-icon>edit</v-icon>
+      </v-list-tile-action>
+    </v-list-tile>
+  </v-list>
+</template>
+
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    }
+  },
+
+  methods: {
+    edit() {}
+  }
+});
+</script>
